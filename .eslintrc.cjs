f1442b1c6f1d34b3e2b1b4f97c42deaa48fac1a1
GivemeYuluJia <1095778225@qq.com>
2024-03-26 16:40:13 +0800

    feat: init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,11 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
@@ -11,5 +16,24 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-plusplus': 'off',
+    // '@typescript-eslint/no-this-alias': 'off',
+    // '@typescript-eslint/no-var-requires': 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-unresolved': 'off',
+    'vuejs-accessibility/form-control-has-label': 'off',
+    'consistent-return': 'off', // 强制统一返回值
+    'no-param-reassign': 'off', // 参数重新分配
+    'no-underscore-dangle': 'off', // 使用下划线命名
+    'comma-spacing': 'off',
+    'vuejs-accessibility/click-events-have-key-events': 'off',
+    'max-len': 'off',
+    'no-unused-expressions': 'off', // 17
+    'linebreak-style': 'off',
+    'vue/multi-word-component-names': 'off', // 开启组件需要多单词
+    'vue/no-setup-props-destructure': 'off',
+    'vuejs-accessibility/anchor-has-content': 'off',
   }
 }
---
+++ src/views/home/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app-home-container">
+    <!-- header -->
+
+    <!-- content -->
+    <main>
+      <!-- 画布区域 -->
+      <div id="workspace">
+        <div class="canvas-box">
+          <div class="inside-shadow"></div>
+          <canvas id="canvas" :class="state.ruler ? 'design-stage-grid' : ''"></canvas>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+<script name="Home" setup lang="ts">
+import { reactive } from 'vue';
+const state = reactive({
+  menuActive: 1,
+  show: false,
+  toolsBarShow: true,
+  attrBarShow: true,
+  select: null,
+  ruler: false,
+});
+</script>
+<style lang="less" scoped>
+.app-home-container {
+  height: 100vh;
+
+  > main {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
